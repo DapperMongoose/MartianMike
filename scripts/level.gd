b8c7999bb40bfff479f22f679c6c5dd5c6ce9727
DapperMongoose <11383629+DapperMongoose@users.noreply.github.com>
2024-04-27 20:52:44 -0500

    completed arrays and groups lesson

--- scripts/level.gd
+++ scripts/level.gd
@@ -3,12 +3,14 @@ extends Node2D
 @onready var start_position = $StartPosition
 @onready var player = $Player
 
+func _ready():
+	var traps = get_tree().get_nodes_in_group("traps")
+
 func _process(delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
-
 
 func _on_deathzone_body_entered(body):
 	reset_player()
